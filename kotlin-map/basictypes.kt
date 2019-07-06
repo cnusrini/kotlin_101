0cb66ae77069cf78c8700c3d290be6a3c63092e5
myopenledger <cnu.srini@hotmail.com>
2019-07-06 16:54:04 -0500

    tutorial on basic types in kotlin

---
+++ kotlin-map/basictypes.kt
@@ -0,0 +1,31 @@
+
+fun main(args: Array<String>) {
+
+   val range: Byte = 112
+    val temperature: Short = -11245
+    val highestScore: Long = 9999
+    val letter: Char
+    letter = 'k'
+    val flag = true
+    val language = "French"
+   val score = 95
+   val fname:String
+    fname = "first name"
+   val age:Int
+    age = 10
+    printvars(flag,range,temperature,highestScore,letter,language,fname,score,age)
+}
+
+fun printvars(flag:Boolean,range:Byte,temperature:Short,highestScore:Long,letter:Char,language:String,fname:String,score:Int,age:Int){
+
+    println(flag)
+    println(range)
+    println(temperature)
+    println(highestScore)
+    println(letter)
+    println(language)
+    println(fname)
+    println(score)
+    println(age)
+
+}
